430c305a9b6aac6354810d70322049838f44230c
ramon-tech <ramonalbert14@gmail.com>
2024-01-24 13:33:36 -0300

    Adicionado javascript e finalizado projeto

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-let primeiroNumero
-let segundoNumero
+function generateNumber(){
 
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    const numberResult = document.querySelector (".number-result")
+    numberResult.textContent = result;
+}
